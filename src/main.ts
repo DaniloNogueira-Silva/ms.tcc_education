00c69be56859c3423d998b10abd2365879a3d929
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-02-26 19:57:53 -0300

    fix: :bug: Corrigindo cors

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://tcc-frontend-flax.vercel.app'],
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
   });
 
   await app.listen(3000);
